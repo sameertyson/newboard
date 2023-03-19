f28a388efda370625baa92d4b78885783d9aad98
sameertyson <mohdsammu44820@gmail.com>
2023-03-19 17:05:22 +0000

    commit message

--- src/components/CowinDashboard/index.js
+++ src/components/CowinDashboard/index.js
@@ -1,1 +1,73 @@
 // Write your code here
+import {Component} from 'react'
+
+import VaccinationCoverage from '../VaccinationCoverage'
+import './index.css'
+
+const fetchState = {
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+  loading: 'LOADING',
+}
+
+class CowinDashboard extends Component {
+  state = {fetchStatus: 'INITIAL', vaccineCoverage: []}
+
+  componentDidMount() {
+    this.getFetchCovidDetail()
+  }
+
+  getFetchCovidDetail = async () => {
+    const response = await fetch('https://apis.ccbp.in/covid-vaccination-data')
+    const data = await response.json()
+    console.log(data)
+    const updateVaccineCoverage = data.last_7_days_vaccination.map(item => ({
+      dose1: item.dose_1,
+      dose2: item.dose_2,
+      vaccineDate: item.vaccine_date,
+    }))
+    this.setState({
+      vaccineCoverage: updateVaccineCoverage,
+      fetchStatus: fetchState.success,
+    })
+  }
+
+  renderSuccessPage = () => {
+    const {vaccineCoverage} = this.state
+    console.log(vaccineCoverage)
+
+    return (
+      <>
+        <VaccinationCoverage coverageList={vaccineCoverage} />
+      </>
+    )
+  }
+
+  renderDisplayPage = () => {
+    const {fetchStatus} = this.state
+    switch (fetchStatus) {
+      case fetchState.success:
+        return this.renderSuccessPage()
+      default:
+        return null
+    }
+  }
+
+  render() {
+    return (
+      <div className="app-con">
+        <div className="main-icon-con">
+          <img
+            className="plus-icon"
+            src="https://assets.ccbp.in/frontend/react-js/cowin-logo.png"
+            alt="website logo"
+          />
+          <p className="main-icon-name">Co-WIN</p>
+        </div>
+        <h1 className="main-heading">CoWIN Vaccination in India</h1>
+        {this.renderDisplayPage()}
+      </div>
+    )
+  }
+}
+export default CowinDashboard
--- src/components/VaccinationCoverage/index.js
+++ src/components/VaccinationCoverage/index.js
@@ -1,1 +1,56 @@
 // Write your code here
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts'
+
+import './index.css'
+
+const VaccinationCoverage = props => {
+  const {coverageList} = props
+  const DataFormatter = number => {
+    if (number > 1000) {
+      return `${(number / 1000).toString()}k`
+    }
+    return number.toString()
+  }
+
+  return (
+    <ResponsiveContainer width="100%" height={500}>
+      <BarChart
+        data={coverageList}
+        margin={{
+          top: 5,
+        }}
+      >
+        <XAxis
+          dataKey="vaccineDate"
+          tick={{
+            stroke: 'gray',
+            strokeWidth: 1,
+          }}
+        />
+        <YAxis
+          tickFormatter={DataFormatter}
+          tick={{
+            stroke: 'gray',
+            strokeWidth: 0,
+          }}
+        />
+        <Legend
+          wrapperStyle={{
+            padding: 30,
+          }}
+        />
+        <Bar dataKey="dose1" name="dose_1" fill="#1f77b4" barSize="20%" />
+        <Bar dataKey="dose2" name="dose_2" fill="#fd7f0e" barSize="20%" />
+      </BarChart>
+    </ResponsiveContainer>
+  )
+}
+
+export default VaccinationCoverage

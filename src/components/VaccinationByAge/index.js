db1450aa2df3fac950b669a570710b4f95266b3d
sameertyson <124371938+sameertyson@users.noreply.github.com>
2023-03-20 10:09:45 +0530

    Update index.js

--- src/components/VaccinationByAge/index.js
+++ src/components/VaccinationByAge/index.js
@@ -1,1 +1,35 @@
 // Write your code here
+import {PieChart, Pie, Legend, Cell, ResponsiveContainer} from 'recharts'
+
+const VaccinationByAge = props => {
+  const {byAgeDetails} = props
+
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <PieChart>
+        <Pie
+          cx="50%"
+          cy="50%"
+          data={byAgeDetails}
+          startAngle={0}
+          endAngle={3600}
+          innerRadius="0%"
+          outerRadius="70%"
+          dataKey="count"
+        >
+          <Cell name="18-44" fill="#2d87bb" />
+          <Cell name="45-60" fill=" #a3df9f" />
+          <Cell name="Above 60" fill="#64c2a6" />
+        </Pie>
+        <Legend
+          iconType="circle"
+          layout="horizontal"
+          horizontalAlign="middle"
+          align="center"
+        />
+      </PieChart>
+    </ResponsiveContainer>
+  )
+}
+
+export default VaccinationByAge
